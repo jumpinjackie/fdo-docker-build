e2c723e94f8a8bb9581452971d82726ec7b2d10f
Jackie Ng <jumpinjackie@gmail.com>
2018-11-11 17:37:38 +1100

    Update build_fdo_all.sh

--- build_fdo_all.sh
+++ build_fdo_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 for $distro in ubuntu16 ubuntu14 centos7 centos6
 do
+    echo "Starting FDO build for $distro"
     time ./docker/fdo/$distro/build/snap.sh 2>&1 | tee logs/fdo_$distro.log
     if [ "$?" -ne 0 ] ; then
         echo "ERROR building FDO for $distro"
