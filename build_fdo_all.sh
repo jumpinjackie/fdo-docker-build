d6e2aa155d82d2668373bc5454c64a7fa5c7c1c1
Jackie Ng <jumpinjackie@gmail.com>
2018-11-15 22:03:29 +1100

    Add CPU directory as another directory level. This rearranges a whole lotta stuff. Hopefully, everything got covered in this move. We'll soon find out.

--- build_fdo_all.sh
+++ build_fdo_all.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-for distro in ubuntu16 ubuntu14 centos7 centos6
+for cpu in x64 x86
 do
-    echo "Starting FDO build for $distro"
-    time ./docker/fdo/$distro/build/snap.sh 2>&1 | tee logs/fdo_$distro.log
-    if [ "$?" -ne 0 ] ; then
-        echo "ERROR building FDO for $distro"
-        exit 1
-    fi
+    for distro in ubuntu16 ubuntu14 centos7 centos6
+    do
+        if [ -d ./docker/$cpu/fdo/$distro ];
+        then
+            echo "Starting FDO build for $distro ($cpu)"
+            time ./docker/$cpu/fdo/$distro/build/snap.sh 2>&1 | tee logs/fdo_${distro}_${cpu}.log
+            if [ "$?" -ne 0 ] ; then
+                echo "ERROR building FDO for $distro ($cpu)"
+                exit 1
+            fi
+        else
+            echo "No such docker environment for $distro ($cpu). Skipping"
+        fi
+    done
 done
---
+++ docker/x64/fdo/centos6/build/snap.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+ORIG=$(pwd)                                                                                                       
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"                                                           
+ROOT="$(realpath $DIR/../../../../..)"
+PROJ="$ROOT/fdo/"
+STAGE="$ROOT/artifacts"
+
+#TODO: This should be sourced from a central place (as we fork this out to distro-specific builds)
+FDO_VER=4.2.0.0
+
+CPU=`basename $(dirname $(dirname $(dirname $DIR)))`
+COMPONENT=`basename $(dirname $(dirname $DIR))`
+DISTRO=`basename $(dirname $DIR)`
+THIS_DIR=`basename $DIR`
+CONTAINER_NAME="${COMPONENT}_${DISTRO}_${THIS_DIR}_${CPU}"
+CCACHE_LOCATION="${ROOT}/caches/${CPU}/${COMPONENT}/${DISTRO}"
+
+indent(){
+    sed 's/^/    /'
+}
+
+echo "Taking a 'develop' snapshot first (required for '$CONTAINER_NAME')"
+"$DIR/../develop/snap.sh" | indent
+
+cd "$DIR"
+docker build . -t "$CONTAINER_NAME:latest"
+if [ "$?" -ne 0 ] ; then
+    exit 1
+fi
+echo "Copying SDK tarball to artifacts"
+docker run --rm -it -v ${STAGE}:/artifacts $CONTAINER_NAME cp /usr/local/src/fdo/build/fdosdk.tar.gz /artifacts/fdosdk-${FDO_VER}-${DISTRO}-${CPU}.tar.gz
+if [ "$?" -ne 0 ] ; then
+    exit 1
+fi
+echo "Copying ccache output to: ${CCACHE_LOCATION}"
+docker run --rm -it -v ${CCACHE_LOCATION}:/tmp/cache $CONTAINER_NAME cp -r /root/.ccache /tmp/cache
+if [ "$?" -ne 0 ] ; then
+    exit 1
+fi
+
+echo "things to try:"
+echo "docker run --rm -it $CONTAINER_NAME /bin/bash"
+echo "docker run --rm -it $CONTAINER_NAME ls /usr/local/src/fdo/build"
+echo
+
+cd "$ORIG"
