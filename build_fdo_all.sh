5ff5b3c9ee171e0f700060fd17287ae13adc16b5
Jackie Ng <jumpinjackie@gmail.com>
2018-11-17 17:39:20 +1100

    Add ubuntu18 build environment for FDO

--- build_fdo_all.sh
+++ build_fdo_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for cpu in x64 x86
 do
-    for distro in ubuntu16 ubuntu14 centos7
+    for distro in ubuntu18 ubuntu16 ubuntu14 centos7
     do
         if [ -d ./docker/$cpu/fdo/$distro ];
         then
